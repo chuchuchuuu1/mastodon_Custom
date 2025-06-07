206e1d8843aea3f9f41f75e0821340360f6fb5e9
chuchu <rntjdrud1@naver.com>
2025-06-07 21:07:51 +0900

    Update branding_controller.rb

--- app/controllers/admin/settings/branding_controller.rb
+++ app/controllers/admin/settings/branding_controller.rb
@@ -6,4 +6,19 @@ class Admin::Settings::BrandingController < Admin::SettingsController
   def after_update_redirect_path
     admin_settings_branding_path
   end
+
+  # === 추가: 강한 파라미터 설정 ===
+  def instance_params
+    params.require(:instance).permit(
+      :header_logo,
+      :favicon,
+      :background_image,
+      :primary_color,
+      :accent_color,
+      :background_color,
+      :theme,
+      :description,
+      :custom_css # <<== 이 줄이 중요!
+    )
+  end
 end
